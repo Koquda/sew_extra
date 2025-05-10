fac6d3fb445c88aa274a3a006e9430e56b0e2f16
uo282874 <uo282874@uniovi.es>
2025-05-10 18:14:02 +0200

    Refactor Noticias class to use News API for fetching articles, update news display format, and change the title to reflect local news about Pesoz.

--- js/noticias.js
+++ js/noticias.js
@@ -1,17 +1,22 @@
 class Noticias {
-    constructor(ulElement, urlRSS) {
+    constructor(ulElement) {
         this.ulElement = ulElement;
-        this.urlRSS = urlRSS;
+        this.apiKey = '4a236f45a50843a5827606a3edb96ed2';
     }
 
     cargarNoticias() {
-        const self = this;
+        const self = this
         $.ajax({
-            url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(this.urlRSS),
+            url: 'https://newsapi.org/v2/everything',
             method: 'GET',
-            dataType: 'json',
+            data: {
+                q: 'Asturias',
+                language: 'es',
+                sortBy: 'popularity',
+                apiKey: this.apiKey
+            },
             success: function(data) {
-                self.mostrarNoticias(data.items);
+                self.mostrarNoticias(data.articles);
             },
             error: function() {
                 $(self.ulElement).html('<li>No se pudieron cargar las noticias.</li>');
@@ -23,17 +28,22 @@ class Noticias {
         // Limpiar el ul
         $(this.ulElement).empty();
         // Insertar título como <li>
-        $(this.ulElement).append('<li><h2>Noticias</h2></li>');
+        $(this.ulElement).append('<li><h2>Noticias sobre Pesoz y los alrededores</h2></li>');
         // Insertar noticias como <li>
         noticias.slice(0,5).forEach(function(noticia) {
-            $(this.ulElement).append(`<li><a href="${noticia.link}" target="_blank">${noticia.title}</a></li>`);
+            $(this.ulElement).append(`
+                <li>
+                    <a href="${noticia.url}" target="_blank">${noticia.title}</a>
+                    <p>${noticia.description}</p>
+                </li>
+            `);
         }, this);
     }
 }
 
 $(document).ready(function() {
-    // Segundo <ul> de <main> para noticias
-    const ulNoticias = $('main ul').eq(1);
-    const noticias = new Noticias(ulNoticias, 'https://www.lne.es/rss/asturias-occidente');
+    // Seleccionar el ul dentro de section dentro de main
+    const ulNoticias = $('main section ul');
+    const noticias = new Noticias(ulNoticias);
     noticias.cargarNoticias();
 });
